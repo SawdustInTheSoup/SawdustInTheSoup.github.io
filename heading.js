fbc9c268f852cc4789815bd19b2d45a3183a73d5
SawdustInTheSoup <67484693+SawdustInTheSoup@users.noreply.github.com>
2020-07-17 10:52:15 -0700

    Create heading.js

---
+++ heading.js
@@ -0,0 +1,13 @@
+var heading = "
+		<div class = "header">
+			<h1 id="main-heading">Sawdust in the Soup</h1>
+		</div>
+		<table class = "links">
+			<tbody>
+				<tr>
+					<td><a href="https://sawdustinthesoup.github.io/recipes">Recipes</a></td>
+					<td><a href="https://sawdustinthesoup.github.io">Home</a></td>
+				</tr>
+			</tbody>
+		</table>
+"
